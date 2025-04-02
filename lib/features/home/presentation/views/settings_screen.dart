f5cce3177d66834c664221394ca30495ee91c561
3mad Hany <144627820+3madhani@users.noreply.github.com>
2025-04-02 20:42:57 +0200

    feat: enhance SettingsScreen UI with new layout and icons for improved user experience

--- lib/features/home/presentation/views/settings_screen.dart
+++ lib/features/home/presentation/views/settings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
@@ -6,20 +7,54 @@ class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Settings'),
-        centerTitle: true,
+      appBar: AppBar(title: const Text('Settings')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            ListTile(
+              minVerticalPadding: 40,
+              title: const Text('Name'),
+              leading: const CircleAvatar(radius: 30),
+              trailing: IconButton(
+                icon: const Icon(Iconsax.scan_barcode),
+                onPressed: () {},
+              ),
+            ),
+            const Card(
+              elevation: 3,
+              child: ListTile(
+                title: Text('Profile'),
+                leading: Icon(Iconsax.user),
+                trailing: Icon(Icons.arrow_forward_ios),
+              ),
+            ),
+            const Card(
+              elevation: 3,
+              child: ListTile(
+                title: Text('Theme'),
+                leading: Icon(Iconsax.color_swatch),
+              ),
+            ),
+
+            Card(
+              elevation: 3,
+              child: ListTile(
+                title: const Text('Dark Mode'),
+                leading: const Icon(Iconsax.moon),
+                trailing: Switch(value: true, onChanged: (value) {}),
+              ),
+            ),
+            const Card(
+              elevation: 3,
+              child: ListTile(
+                title: Text('Sign Out'),
+                trailing: Icon(Iconsax.logout_1),
+              ),
+            ),
+          ],
+        ),
       ),
-      body: const Center(
-        child: Text('Settings Screen'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Add your action here
-        },
-        child: const Icon(Icons.add),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
