dc09f9f93087981e869babaa19a549f9b1aa2980
3mad Hany <144627820+3madhani@users.noreply.github.com>
2025-04-02 22:30:36 +0200

    feat: add color picker dialog to SettingsScreen for customizable options

--- lib/features/home/presentation/views/settings_screen.dart
+++ lib/features/home/presentation/views/settings_screen.dart
@@ -1,5 +1,6 @@
 import 'package:chitchat/features/settings/presentation/views/profile_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:iconsax/iconsax.dart';
 
 class SettingsScreen extends StatelessWidget {
@@ -45,6 +46,28 @@ class SettingsScreen extends StatelessWidget {
             Card(
               elevation: 3,
               child: ListTile(
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder:
+                        (context) => AlertDialog(
+                          content: SingleChildScrollView(
+                            child: BlockPicker(
+                              pickerColor: Colors.red,
+                              onColorChanged: (value) {},
+                            ),
+                          ),
+                          actions: [
+                            TextButton(
+                              child: const Text('Done'),
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                            ),
+                          ],
+                        ),
+                  );
+                },
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10),
                 ),
